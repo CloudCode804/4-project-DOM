5f3417d6d6c7425ca7efefb432f2776df3fe9e98
CloudCode804 <cloudcodeide@gmail.com>
2024-03-20 14:03:05 +0500

    main

--- 1-project /script.js
+++ 1-project /script.js
@@ -1,13 +1,21 @@
 const btnEl=document.querySelectorAll(".button")
-let hashValue="0123456789ABCDEF"
+// let hashValue="0123456789ABCDEF"
+// let hash="#"
+// for(let i=0;i<6;i++){
+    
+//     hash+=hashValue[Math.floor(Math.random()*hashValue.length)]
+    
+// }
+
+console.log(hash)
+btnEl.forEach((item)=>{
+    let hashValue="0123456789ABCDEF"
 let hash="#"
 for(let i=0;i<6;i++){
     
     hash+=hashValue[Math.floor(Math.random()*hashValue.length)]
     
 }
-console.log(hash)
-btnEl.forEach((item)=>{
     item.setAttribute("id",hash)
     console.log(item)
 
