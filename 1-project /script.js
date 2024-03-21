8f5f581c7a179bbd7cb231fc85c5e7a05eda9a4c
CloudCode804 <cloudcodeide@gmail.com>
2024-03-21 10:15:01 +0500

    main

--- 1-project /script.js
+++ 1-project /script.js
@@ -1,16 +1,21 @@
 const btnEl=document.querySelectorAll(".button")
 
-function uniqueIDgenerated(){
-    btnEl.forEach((item)=>{
-        let hashValue="0123456789ABCDEF"
+function randomIdGenerate(){
+    let hashValue="0123456789ABCDEF"
     let hash="#"
     for(let i=0;i<6;i++){
         hash+=hashValue[Math.floor(Math.random()*hashValue.length)]
     }
-        item.setAttribute("id",hash)
+    return hash
+}
+console.log(randomIdGenerate())
+function uniqueIDgenerated(){
+    btnEl.forEach((item)=>{
+     
+        item.setAttribute("id",randomIdGenerate())
         console.log(item)
     })
-    return hash
+   
 }
 
 console.log(uniqueIDgenerated())
