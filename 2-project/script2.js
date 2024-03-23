31c2a597207ad5595d7616669e78e39d4a4aaaeb
CloudCode804 <cloudcodeide@gmail.com>
2024-03-23 12:12:58 +0500

    main

--- 2-project/script2.js
+++ 2-project/script2.js
@@ -7,17 +7,22 @@ form.addEventListener("submit",function(e){
     const result=document.querySelector("#result")
 
     if (height === "" || height<0|| isNaN(height)){
-        result.innerHTML=` pleace valid a  Height ${height}`
+        result.innerHTML=` Pleace Valid a  Height & Weight `
+        result.style.color = "red";
     } else if(weight === "" || weight<0|| isNaN(weight)){
-        result.innerHTML=` pleace valid a  Height ${weight}`
+        result.innerHTML=` Pleace Valid a  Height & Weight `
+        result.style.color = "red";
     }else{
         const bmi=(weight/((height*height)/10000)).toFixed(2)
         if(bmi < 18.6){
             result.innerHTML=`<span>Your are in Under wight ${bmi}</span>`
+            result.style.color = "black";
         }else if(bmi < 24.9){
             result.innerHTML=`<span>Your are in Normal wight ${bmi}</span>`
+            result.style.color = "black";
         }else{
             result.innerHTML=`<span>Your are in OverWeight  ${bmi}</span>`
+            result.style.color = "black";
         }
     }
 
